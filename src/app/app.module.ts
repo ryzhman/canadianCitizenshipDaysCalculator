48b925a01cb2046439231c3746eca24ba8b7a6c5
Oleksandr Ryzhkov <ryzhman@gmail.com>
2021-01-16 17:44:42 -0500

    Header component has navigation links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HeaderModule} from './header/header.module';
 import {HeaderComponent} from './header/header/header.component';
+import {SharedModule} from './shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
-    HeaderModule
+    HeaderModule,
+    SharedModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [HeaderComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,11 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './header/header.component';
+import {LandingDateModule} from '../landing-date/landing-date.module';
+import {RouterModule} from '@angular/router';
+import {AboutComponent} from '../about/about/about.component';
+import {LandingDateStageComponent} from '../landing-date/landing-date-selector/landing-date-stage.component';
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    LandingDateModule,
+    RouterModule,
+    RouterModule.forRoot([
+      {path: 'about', component: AboutComponent},
+      {path: 'calculate-date', component: LandingDateStageComponent},
+      {path: '', redirectTo: 'about', pathMatch: 'full'},
+      {path: '**', redirectTo: 'about', pathMatch: 'full'}
+    ]),
   ]
 })
-export class HeaderModule { }
+export class HeaderModule {
+}
