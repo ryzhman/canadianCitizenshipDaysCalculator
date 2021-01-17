52af5ecff992f9f42146de63219e977a52349f4e
Oleksandr Ryzhkov <ryzhman@gmail.com>
2021-01-17 16:12:00 -0500

    TripService created to provide trips, currently trips are fetched from the local file

---
+++ src/models/trip.ts
@@ -0,0 +1,7 @@
+export interface Trip {
+  id?: number;
+  country: string;
+  departureDate: Date;
+  arrivalDate: Date;
+  notes: string;
+}
---
+++ src/services/trip.service.ts
@@ -0,0 +1,48 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {Trip} from '../models/Trip';
+import {catchError, map, tap} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TripService {
+  private productUrl = 'api/trips.json';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getTrips(): Observable<Trip[]> {
+    return this.http.get<Trip[]>(this.productUrl)
+      .pipe(
+        tap(data => {
+          console.log('All: ' + JSON.stringify(data));
+        }),
+        catchError(this.handleError)
+      );
+  }
+
+  getProduct(id: number): Observable<Trip | undefined> {
+    return this.getTrips()
+      .pipe(
+        map((products: Trip[]) => products.find(p => p.id === id))
+      );
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
+}
