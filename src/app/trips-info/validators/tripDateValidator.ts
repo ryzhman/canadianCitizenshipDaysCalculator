27e7d52699b2998b3dec88185b222a4009a3a847
Oleksandr Ryzhkov <ryzhman@gmail.com>
2021-01-27 19:59:09 -0500

    Required fields in newTrip modal are marked properly; Autocomplete is turned off for all forms

--- src/app/trips-info/validators/tripDateValidator.ts
+++ src/app/trips-info/validators/tripDateValidator.ts
@@ -7,27 +7,30 @@ import {DateUtils} from '../../../services/utils/dateUtils';
  * @param control invalidateTripDates
  */
 export const tripDateValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-  if (!control.get('departureDate').value || !control.get('arrivalDate').value) {
+  const departureDateForm = control.get('departureDate');
+  const arrivalDateForm = control.get('arrivalDate');
+  if ((!departureDateForm.touched && !departureDateForm.value)
+    || (!arrivalDateForm.touched && !arrivalDateForm.value)) {
     // the form is not filled up yet
     return null;
   }
   // in moment months are zero-index based
   const departureDate = DateUtils.createMomentDate(
-    control.get('departureDate').value.year,
-    control.get('departureDate').value.month - 1,
-    control.get('departureDate').value.day);
+    departureDateForm.value.year,
+    departureDateForm.value.month - 1,
+    departureDateForm.value.day);
   const arrivalDate = DateUtils.createMomentDate(
-    control.get('arrivalDate').value.year,
-    control.get('arrivalDate').value.month - 1,
-    control.get('arrivalDate').value.day);
+    arrivalDateForm.value.year,
+    arrivalDateForm.value.month - 1,
+    arrivalDateForm.value.day);
   const areValidDates = departureDate.isBefore(arrivalDate);
 
   if (!areValidDates) {
-    control.get('departureDate').setErrors({invalidateTripDates: true});
-    control.get('arrivalDate').setErrors({invalidateTripDates: true});
+    departureDateForm.setErrors({invalidateTripDates: true});
+    arrivalDateForm.setErrors({invalidateTripDates: true});
   } else {
-    control.get('departureDate').setErrors(null);
-    control.get('arrivalDate').setErrors(null);
+    departureDateForm.setErrors(null);
+    arrivalDateForm.setErrors(null);
   }
   return areValidDates ? null : {invalidateTripDates: true};
 };
